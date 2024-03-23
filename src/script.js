7401637642c68385cd84f67e605428d8941fc56a
rbarbazz <42906704+rbarbazz@users.noreply.github.com>
2024-03-23 09:11:42 -0700

    Markup with mock data

--- src/script.js
+++ src/script.js
@@ -1,5 +1,38 @@
 import Alpine from 'alpinejs'
+import { format, subDays, getTime } from 'date-fns'
+import persist from '@alpinejs/persist'
+
+Alpine.plugin(persist)
+
+Alpine.data('lastSevenDays', () => ({
+  days: [],
+
+  init() {
+    for (let i = 0; i < 7; i++) {
+      const date = subDays(new Date(), i)
+      date.setHours(0, 0, 0, 0)
+
+      this.days.push(getTime(date))
+    }
+  },
+
+  formatDate(date) {
+    return format(date, 'EEEE')
+  },
+}))
+Alpine.data('progress', function () {
+  return {
+    records: this.$persist({}),
+
+    addRecord(day, value) {
+      this.records[day] = parseInt(value)
+    },
+
+    getValue(day) {
+      return this.records[day] || 0
+    },
+  }
+})
 
 window.Alpine = Alpine
-
 Alpine.start()
